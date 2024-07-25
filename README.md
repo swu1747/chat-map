e9cfce0498e08a4909ef807ea96a861ac8f680c4
swu1747 <swu1747@gmail.com>
2024-07-25 18:19:35 -0400

    save

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# chat-map
-first commit 
+# 🚀 Welcome to your new awesome project!
+
+This project has been created using **webpack-cli**, you can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: path.resolve(__dirname, './client/index.js'),
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, './client/dist'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            filename: 'index.html'
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+
+
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
