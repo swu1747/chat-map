352362272d8c5e6d58583aa2b264faee5f422ec7
swu1747 <swu1747@gmail.com>
2024-07-26 11:46:30 -0400

    save

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-
-const app = express()
-app.use(express.static(path.join(__dirname, '../client/dist')))
-
-app.listen(1234, () => {
-    console.log(1234)
-})
