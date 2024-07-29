ac51811bedf52639d545b37185a4cede53aae43e
swu1747 <swu1747@gmail.com>
2024-07-29 14:03:30 -0400

    save

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,6 +16,7 @@ const config = {
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
+
     devServer: {
         open: true,
         host: 'localhost',
@@ -36,8 +37,13 @@ const config = {
                 exclude: ['/node_modules/'],
             },
             {
+                test: /\.m?js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
+            {
                 test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
+                use: [stylesHandler, 'css-loader'],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -56,10 +62,10 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
+
+
         config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
+
     } else {
         config.mode = 'development';
     }
