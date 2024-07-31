c5fa3bcf031da964bee9b609401b95556d3cb52f
swu1747 <swu1747@gmail.com>
2024-07-31 12:36:12 -0400

    save

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => {
-    return (<>dddd</>)
+    return (<><h1 className="text-3xl font-bold underline">ssssss</h1></>)
 }
 
 export default App
